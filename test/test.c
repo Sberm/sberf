1fe61790330f1c730244f62718d3af331a2364b4
Howard Chu <howardchu95@gmail.com>
2024-10-28 18:48:25 -0700

    sberf test: Comments cleanup

--- test/test.c
+++ test/test.c
@@ -31,12 +31,14 @@ void test_usym()
 		DS("Failed to load userspace symbols from test")
 
 	printf("usym_tb size: %d\n", usym_tb->length);
+
 	/* only test first dso */
 	const struct dso dso_p = usym_tb->dsos[0];
 	printf("usym_tb first dso number of symbols: %d\n", dso_p.length);
 	for (size_t i = 0;i < dso_p.length;i++) {
 		printf("%llx %s\n", dso_p.sym[i].addr, dso_p.sym[i].name);
 	}
+
 	/* sa 55e8c50bf000 ea 55e8c50dc000 */
 	/* 55e8c50e1da0 55e8c51939f0*/
 	printf("sa %llx ea %llx\n", dso_p.start_addr, dso_p.end_addr);
@@ -59,20 +61,8 @@ void test_addr_to_name()
 	ksym_addr_to_sym(ksym_tb, addr, name);
 	printf("kernel symbol: %s\n" ,name);
 
-	/*const struct dso dso_p = usym_tb->dsos[0];*/
-	/*printf("dso_p's info: s_a %llx e_a %llx\n", dso_p.start_addr, dso_p.end_addr);*/
-
-	/* supposed to be 55e8c4de0ef7 ngx_hash_init+0x2b7, but got ngx_write_channel */
-	/*addr = 0x55e8c4de0ef7;*/
-
 	char temp[256];
 
-	// addr = usym_tb->dsos[0].sym[17].addr;
-	// addr += usym_tb->dsos[0].start_addr;
-	// strcpy(temp, usym_tb->dsos[0].sym[17].name);
-	// printf("searching [%s]\n", temp);
-	// 0x55E8C4DE0C40
-	
 	addr = 0x26c40;
 	addr += usym_tb->dsos[0].start_addr;
 	printf("%llx\n",addr);
@@ -83,7 +73,6 @@ void test_addr_to_name()
 
 void test_random() {
 	/* 8 8 */
-	// printf("%d %d\n", sizeof(unsigned long), sizeof(unsigned long long));
 	int len = 20;
 	char *p = malloc(len);
 	strcpy(p + 10, "fuck");
@@ -99,11 +88,12 @@ void test_random() {
 
 int main() 
 {
-	// test_ksym();
- 	// test_usym();
-	// test_addr_to_name();
-	// test_random();
-	DS("%d\n", 1);
-	
+	test_ksym();
+
+	test_usym();
+
+	test_addr_to_name();
+
+	test_random();
 	return 0;
 }
